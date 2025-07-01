e6930929260478f8639ed583681110690dd6efec
komal Dattatraya kumbhar <komalavadhutkumbhar68@gmail.com>
2025-07-01 19:53:54 +0530

    for show links

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -73,7 +73,7 @@ const CoursesPage = () => {
                     Web Development ,Data Analytics ,and Soft Skills Development, provideng specislized training for career enhancement.
                 </p>
                 <center> <p>Have question about specific course ?</p></center><br/>
-                <center> <a href="/contact" className="btn"> Inquire About Courses</a></center>
+                <center> <a   className="btn" href="/contact"> Inquire About Courses</a></center>
                 
         </div>
      )
